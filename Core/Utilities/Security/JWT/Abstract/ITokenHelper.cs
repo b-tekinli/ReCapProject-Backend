2e242c5bd21c451ad78ca71d4791c7e6cfa67024
b-tekinli <btbtbt664@gmail.com>
2021-03-08 15:47:28 +0300

    JWT added

---
+++ Core/Utilities/Security/JWT/Abstract/ITokenHelper.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Security.JWT.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.JWT.Abstract
+{
+    public interface ITokenHelper
+    {
+        AccessToken CreateToken(User user, List<OperationClaim> operationClaims);
+    }
+}
